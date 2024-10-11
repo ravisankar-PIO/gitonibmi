4072966b123e001f857dd5162d0f5c8377c5500f
Ravisankar Pandian <ravisankar.pandian@programmers.io>
2024-10-11 12:15:58 +0530

    update

--- profilesetup.sh
+++ profilesetup.sh
@@ -28,10 +28,10 @@ createprofile(){
   cd .. && echo "export PATH=/QOpenSys/QIBM/ProdData/JavaVM/jdk17/64bit/bin:/QOpenSys/pkgs/bin:$PATH" >> .profile
   
   # Setup gitprompt on bash
-  wget --show-progress https://raw.githubusercontent.com/ravisankar-PIO/gitonibmi/main/gitprompt.sh
+  /QOpenSys/pkgs/bin/wget --show-progress https://raw.githubusercontent.com/ravisankar-PIO/gitonibmi/main/gitprompt.sh
   mv gitprompt.sh .gitprompt.sh
   echo "PROMPT_COMMAND='__posh_git_ps1 \"\${VIRTUAL_ENV:+(\`basename \$VIRTUAL_ENV\`)}\\[\\e[32m\\]\\u\\[\\e[0m\\]@\\h:\\[\\e[33m\\]\\w\\[\\e[0m\\] \" \"\\\\\\\$ \";'\$PROMPT_COMMAND" >> .profile
-  echo "source /home/$current_user/.gitprompt.sh" >> .profile
+  echo "source /home/$1/.gitprompt.sh" >> .profile
 
   # Change the shell to Bash for this user
   /QOpenSys/pkgs/bin/chsh -s /QOpenSys/pkgs/bin/bash $1
